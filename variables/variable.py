db48e71ade4480a13bcad028b531005dc3774a5e
caseychester7414 <60857758+caseychester7414@users.noreply.github.com>
2021-08-10 18:10:58 -0700

    FSW week 2 Assignment 1

---
+++ variables/variable.py
@@ -0,0 +1,7 @@
+failed_subjects="2"
+name="john"
+print("dear Mrs badger")
+print("Your son" + name+ "is Failing" + failed_subjects + "subjects.")
+print(name + "will need to redo" + failed_subjects + "courses.")
+name="Eric"
+print(name + "is doing well in geography.")
